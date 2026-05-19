171458c33683111bf27c2cdd51df128a9d3c5774
Sebastián Torres <seba.torresz@duocuc.cl>
2026-05-19 10:59:40 -0400

    fix test

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -20,6 +20,5 @@ describe('MainContent', () => {
     const button = screen.getByRole('button');
     await userEvent.click(button);
     expect(screen.getByTestId('help-area')).toBeInTheDocument();
-    //expect(screen.getByRole('textbox')).toBeInTheDocument(); //borrar
   });
 });
